46a62ccba91de0a5ffc37ab5dbe47bd96f77c9a5
huntexjames <fredrikjames@gmail.com>
2020-08-29 14:21:23 +0800

    Link Params and redirect packtpub front to back

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,41 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
-  const { branding } = props;
-  return (
-    <nav
-      className="navbar navbar-expand-sm 
+    const { branding } = props;
+    return (
+        <nav
+            className="navbar navbar-expand-sm 
         navbar-dark 
         bg-danger mb-3 py-0"
-    >
-      <div className="container">
-        <a href="/" className="navbar-brand">
-          {branding}
-        </a>
-        <div>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a href="/" className="nav-link">
-                Home
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+        >
+            <div className="container">
+                <a href="/" className="navbar-brand">
+                    {branding}
+                </a>
+                <div>
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">
+                                <i className="fas fa-home">Home</i>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/contact/add" className="nav-link">
+                                <i className="fas fa-plus">Add</i>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-link">
+                                <i className="fas fa-question">About</i>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 //default properties
 Header.defaultProps = {
-  branding: "My App",
+    branding: "My App",
 };
 
 //Validation
 Header.propTypes = {
-  //making sure that branding is string
-  branding: PropTypes.string.isRequired,
+    //making sure that branding is string
+    branding: PropTypes.string.isRequired,
 };
 
 // const headingStyle = {
