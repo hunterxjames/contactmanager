5b07311382c10cb3823604422f9ae31147f84a59
huntexjames <fredrikjames@gmail.com>
2020-08-29 17:40:40 +0800

    Updates

---
+++ precache-manifest.8b015d9e752430dd8252fcac95e8f7ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14846b51df1e7f79b920ccb4db29a6a6",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8996df5b05e3451ec704",
+    "url": "/contactmanager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "32a9e57d2896fbee1936",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8996df5b05e3451ec704",
+    "url": "/contactmanager/static/js/2.9256ba54.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.9256ba54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a9e57d2896fbee1936",
+    "url": "/contactmanager/static/js/main.377442e4.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
